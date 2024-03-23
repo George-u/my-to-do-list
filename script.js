15d9fbf842aebf4c3ca9aee16fb79a8766fc5289
Yuriy Motyl <yuriy.motyl@gmail.com>
2024-03-23 12:48:58 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+/// Add an event listener to the input field for the "keypress" event
+document
+  .getElementById("myInput")
+  .addEventListener("keypress", function (event) {
+    // Check if the Enter key was pressed
+    if (event.key === "Enter") {
+      // Call the newElement() function to create a new list item
+      newElement();
+    }
+  });
+
+// Function to create a new list item
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  // Create a close button and append it to the new list item
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  // Add functionality to hide the current list item when clicking on the close button
+  span.onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
